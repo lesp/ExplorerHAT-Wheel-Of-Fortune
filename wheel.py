1be283a076624ec69eb2d971510f449f5421cdc4
Les Pounder <lespounder@gmail.com>
2015-05-29 15:36:53 +0100

    Updated for EH Pro

---
+++ wheel.py
@@ -0,0 +1,17 @@
+import explorerhat
+from time import sleep
+from random import randint
+
+def wheel(channel, event):
+    explorerhat.light.blink(0.5,0.5)
+        for i in range(5):
+            duration = randint(1,10)
+    print(duration)
+    explorerhat.motor.one.forward(100)
+    sleep(spin)
+    explorerhat.motor.one.stop()
+    explorerhat.light.off()
+
+
+explorerhat.touch.one.pressed(wheel)
+  
