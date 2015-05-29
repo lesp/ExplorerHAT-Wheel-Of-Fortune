442ff9acf2f332774cfa5fdc30f4c8c85b42056f
Les Pounder <lespounder@gmail.com>
2015-05-29 14:48:23 +0000

    Updated code to fix typo

--- wheel.py
+++ wheel.py
@@ -4,11 +4,11 @@ from random import randint
 
 def wheel(channel, event):
     explorerhat.light.blink(0.5,0.5)
-        for i in range(5):
-            duration = randint(1,10)
+    for i in range(5):
+        duration = randint(1,10)
     print(duration)
     explorerhat.motor.one.forward(100)
-    sleep(spin)
+    sleep(duration)
     explorerhat.motor.one.stop()
     explorerhat.light.off()
 
