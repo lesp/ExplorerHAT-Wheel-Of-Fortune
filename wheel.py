b2cbdd1a02c49ac11cc53db273d5e26f602a3392
Les <lespounder@gmail.com>
2015-07-13 08:46:27 +0100

    Removed for loop

--- wheel.py
+++ wheel.py
@@ -4,8 +4,7 @@ from random import randint
 
 def wheel(channel, event):
     explorerhat.light.blink(0.5,0.5)
-    for i in range(5):
-        duration = randint(1,10)
+    duration = randint(1,10)
     print(duration)
     explorerhat.motor.one.forward(100)
     sleep(duration)
